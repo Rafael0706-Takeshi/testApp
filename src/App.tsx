6d0f02e3a60e65eee28412c4cadb7a2c6a099c03
Rafael0706-Takeshi <Rafael07061993@gmail.com>
2025-02-08 14:49:19 +0900

    Router implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import "./App.css";
 import Navbar from "./pages/Navbar";
 import Header from "./pages/Header";
 import Homepage from "./pages/Homepage";
+import Faqpage from "./pages/Faqpage";
+import Tokenpage from "./pages/Tokenpage";
+import Revenuepage from "./pages/Revenuepage";
 import Footer from "./pages/Footer";
+
+import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
-      <Homepage />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="faqs" element={<Faqpage />} />
+          <Route path="token" element={<Tokenpage />} />
+          <Route path="revenue-share" element={<Revenuepage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet /> {/* This will render the current page content */}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
